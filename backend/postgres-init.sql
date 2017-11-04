79d34d123c2840c2e3593968f517cf78db498cd2
luyor <luyuhelyh@gmail.com>
2017-11-04 15:29:48 -0700

    added db schema for book,node,userinfo

--- backend/postgres-init.sql
+++ backend/postgres-init.sql
@@ -1,8 +1,24 @@
-Create Table tableName (
+CREATE TABLE book (
     id serial primary key,
-    fieldName text not null,
-    created timestamptz default now()
+    title text NOT NULL,
+    cover_image text,
+    description text,
+    root_node_id integer NOT NULL
 );
 
-INSERT INTO tableName (fieldName) VALUES ('test data 1');
-INSERT INTO tableName (fieldName) VALUES ('test data 2');
+CREATE TABLE node (
+    id serial primary key,
+    user_id integer NOT NULL,
+    book_id integer NOT NULL,
+    parent_id integer,
+    like_sum integer NOT NULL default 0,
+    images text[],
+    create_date timestamp NOT NULL default current_timestamp
+);
+
+CREATE TABLE userinfo (
+    id serial primary key,
+    username text NOT NULL,
+    password text NOT NULL,
+    create_date timestamp NOT NULL default current_timestamp
+);
