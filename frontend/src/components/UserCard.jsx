bb2b4182746da6786d2ed2d385ea0f1e6de56ac8
rma61 <rma61@sfu.ca>
2017-11-06 15:58:27 -0800

    css adusting and centering the user card

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -22,33 +22,29 @@ UserName.propTypes = {
 export function UserCard(props) {
   return (
     <div className="name-card-wrapper">
-      <div className="row valign-wrapper">
-        <div className="col s6 offset-s3 valign">
-          <div className="card">
-            <div className="card-content">
-              <span id="user_card_title" className="card-title center-align"> {props.isRegister ? 'Create Account' : 'Login'} </span>
+      <div className="card">
+        <div className="card-content">
+          <span id="user_card_title" className="card-title center-align"> {props.isRegister ? 'Create Account' : 'Login'} </span>
+          <div className="row">
+            <form className="col s12">
+              <UserName isRegister={props.isRegister} />
               <div className="row">
-                <form className="col s12">
-                  <UserName isRegister={props.isRegister} />
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input id="email" type="email" className="validate" />
-                      <label htmlFor="email">Email Address</label>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input id="password" type="password" className="validate" />
-                      <label htmlFor="password">Password</label>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="row">
-                <div className="col s12">
-                  <a id="user_card_btn" className="waves-effect waves-light btn">{props.isRegister ? 'Create Account' : 'Login'}</a>
+                <div className="input-field col s12">
+                  <input id="email" type="email" className="validate" />
+                  <label htmlFor="email">Email Address</label>
                 </div>
               </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input id="password" type="password" className="validate" />
+                  <label htmlFor="password">Password</label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="row">
+            <div className="col s12">
+              <a id="user_card_btn" className="waves-effect waves-light btn">{props.isRegister ? 'Create Account' : 'Login'}</a>
             </div>
           </div>
         </div>
