b972ed5fdf6c8817bb78a2e1a011345e3209b9d7
rma61 <rma61@sfu.ca>
2017-11-06 00:46:23 -0800

    basic register login complete

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export class LoginPage extends React.Component {
   render() {
     return (
-      <div className="register-wrapper">
+      <div className="login-wrapper">
         <div className="card">
           <div className="card-content">
             <span className="card-title">Login </span>
