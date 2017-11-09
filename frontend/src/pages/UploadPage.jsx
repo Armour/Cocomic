ce274f8c825d3bf544c1d597d2176de33daf4782
zacharyl <zacharyl@sfu.ca>
2017-11-09 10:49:08 -0800

    add test pages in local branch

---
+++ frontend/src/pages/UploadPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FileUploadBox } from 'components/FileUploadBox';
+
+export class UploadPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <FileUploadBox />
+      </div>
+    );
+  }
+}
