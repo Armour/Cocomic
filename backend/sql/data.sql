242b19541ff898c2c9bce169ce40fac66f8343fa
Jingda Chen <holaelmundokingston@gmail.com>
2017-11-22 14:49:51 -0800

    add more data

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -2,6 +2,24 @@ INSERT INTO userinfo(username, email, password) VALUES('Kingston', 'k@k.com', 'd
 INSERT INTO book(title, cover_image, description, root_chapter_id)
     VALUES('Conan Escalar Montañas', 'sample-1', 'Conan escalar montañas todos los sábados.', 1);
 
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_1', 'Conan escalar montañas todos los sábados.', 5);
+
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_2', 'Conan escalar montañas todos los sábados.', 7);
+
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_3', 'Conan escalar montañas todos los sábados.', 9);
+
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_4', 'Conan escalar montañas todos los sábados.', 11);
+
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_5', 'Conan escalar montañas todos los sábados.', 13);
+
+INSERT INTO book(title, cover_image, description, root_chapter_id)
+    VALUES('Conan Escalar Montañas', 'conan_cover_6', 'Conan escalar montañas todos los sábados.', 15);
+
 INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
     VALUES(1, 1, 'Conan Prepara el Equipaje', 'Conan hace la maleta y reserve un habitación individual', 23, '{"sample-1", "sample-1"}');
 
@@ -13,3 +31,41 @@ INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, i
 
 INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
     VALUES(1, 1, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 3, 15, '{"sample-1", "sample-1"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 2, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 2, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 5, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 3, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 3, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 7, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 4, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 4, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 9, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 5, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 5, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 11, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 6, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 6, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 13, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, like_sum, images)
+    VALUES(1, 7, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, '{"chapter_1_1", "chapter_1_2", "chapter_1_3", "chapter_1_4"}');
+
+INSERT INTO chapter(user_id, book_id, title, description, parent_id, like_sum, images)
+    VALUES(1, 7, 'Conan compra un pez', 'Conan ve a hombre qien está pescando, y compra un pez de él.', 15, 15, '{"chapter_2_1", "chapter_2_2", "chapter_2_3", "chapter_2_4"}');
+
+
