e195ddeb0b032738e79a20c526f39ca713301c3e
luyor <luyuhelyh@gmail.com>
2017-11-24 22:11:09 -0800

    fix sidenav link

--- frontend/src/components/Sidenav.jsx
+++ frontend/src/components/Sidenav.jsx
@@ -6,11 +6,11 @@ export class Sidenav extends React.Component {
     return (
       <ul id="slide-out" className="side-nav">
         <li><NavLink exact to="/"><i className="material-icons">home</i>Home</NavLink></li>
-        <li><NavLink to="popular"><i className="material-icons">thumb_up</i>Popular</NavLink></li>
-        <li><NavLink to="new"><i className="material-icons">fiber_new</i>New</NavLink></li>
-        <li><NavLink to="about"><i className="material-icons">info</i>About us</NavLink></li>
-        <li><NavLink to="login"><i className="material-icons">person_outline</i>Log in</NavLink></li>
-        <li><NavLink to="register"><i className="material-icons">person_add</i>Register</NavLink></li>
+        <li><NavLink to="/popular"><i className="material-icons">thumb_up</i>Popular</NavLink></li>
+        <li><NavLink to="/new"><i className="material-icons">fiber_new</i>New</NavLink></li>
+        <li><NavLink to="/about"><i className="material-icons">info</i>About us</NavLink></li>
+        <li><NavLink to="/login"><i className="material-icons">person_outline</i>Log in</NavLink></li>
+        <li><NavLink to="/register"><i className="material-icons">person_add</i>Register</NavLink></li>
       </ul>
     );
   }
