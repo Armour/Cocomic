4599a8d3a6bb0acf1a2cca4f6e57f233320db6ae
Chong Guo <armourg@sfu.ca>
2017-11-27 20:05:34 -0800

    added preloader for ImageLoader

--- frontend/src/components/ImageLoader.jsx
+++ frontend/src/components/ImageLoader.jsx
@@ -10,6 +10,31 @@ export class ImageLoader extends React.Component {
   }
 
   render() {
+    const frameStyle = {
+      height: '200px',
+    };
+    const loaderStyle = {
+      top: '50%',
+    };
+    if (this.props.img_data === '') {
+      return (
+        <div className="center" style={frameStyle}>
+          <div className="preloader-wrapper active" style={loaderStyle}>
+            <div className="spinner-layer spinner-blue-only">
+              <div className="circle-clipper left">
+                <div className="circle" />
+              </div>
+              <div className="gap-patch">
+                <div className="circle" />
+              </div>
+              <div className="circle-clipper right">
+                <div className="circle" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <img src={`data:image/jpeg;base64,${this.props.img_data}`} alt={this.props.alt} />
     );
