faa6186cce0a0b9ea90205f3f53ab5641b6d4124
Chong Guo <armourg@sfu.ca>
2017-11-29 23:43:41 -0800

    added editChapter

--- backend/controllers/chapter.js
+++ backend/controllers/chapter.js
@@ -79,19 +79,61 @@ export const addChapter = async (req, res) => {
     } = req.body;
     const { uid: userId } = req.session;
     const chapterImages = uploadImages(images);
+    if (chapterImages === undefined || chapterImages.length === 0) {
+      throw Error('Should have at least one image for new chapter');
+    }
     const chapterQuery = `
     INSERT INTO chapter(user_id, book_id, title, description, parent_id, images)
     VALUES ($1, $2, $3, $4, $5, $6) RETURNING id
     `;
-    if (chapterImages === undefined || chapterImages.length === 0) {
-      throw Error('Should have at least one image for new chapter');
-    }
     const chapterQueryValues = [userId, bookId, title, description, parentId, chapterImages];
     const { rows } = await db.query(chapterQuery, chapterQueryValues);
     if (rows !== undefined && rows.length !== 0) {
       return res.json({ chapterId: rows[0].id, images: chapterImages });
     }
     throw Error('insert failed');
+  } catch (e) {
+    return res.status(404).json({ message: e.message });
+  }
+};
+
+export const editChapter = async (req, res) => {
+  try {
+    const {
+      chapterId,
+      title,
+      description,
+      images,
+    } = req.body;
+    const { uid: userId } = req.session;
+    const chapterImages = uploadImages(images);
+    if (chapterImages === undefined || chapterImages.length === 0) {
+      throw Error('Should have at least one image for new chapter');
+    }
+    const updateQuery = `
+    UPDATE chapter
+    SET (title, description, images) = ($1, $2, $3)
+    WHERE id=$4 AND user_id=$5
+    RETURNING title as new_title, description as new_description, images as new_images
+    `;
+    const updateQueryValues = [title, description, chapterImages, chapterId, userId];
+    const { rows } = await db.query(updateQuery, updateQueryValues);
+    if (rows !== undefined && rows.length !== 0) {
+      const newTitle = rows[0].new_title;
+      const newDescription = rows[0].new_description;
+      const newImages = rows[0].new_images;
+      if (title === newTitle &&
+          description === newDescription &&
+          JSON.stringify(chapterImages) === JSON.stringify(newImages)) {
+        return res.json({
+          chapterId,
+          title: newTitle,
+          description: newDescription,
+          images: newImages,
+        });
+      }
+    }
+    throw Error('update failed');
   } catch (e) {
     return res.status(404).json({ message: e.message });
   }
